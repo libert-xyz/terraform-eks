107d049e4244379d5be3f41b70f15c7c5fb00d94
Libert <libert@nuvops.com>
2019-12-02 18:57:12 -0500

    add VPC

---
+++ eks.tf
@@ -0,0 +1,82 @@
+terraform {
+  backend "remote" {
+    organization = "nuvops"
+
+    workspaces {
+      name = "terraform-eks-sample"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+variable "cluster-name" {
+    default = "terraform-eks-sample"
+    type    = string
+}
+
+##############
+##RESOURCES###
+##############
+
+ data "aws_availability_zones" "available" {
+ }
+
+ resource "aws_vpc" "demo" {
+   cidr_block = "10.0.0.0/16"
+
+   tags = {
+     "Name"                                      = "terraform-eks-demo-node"
+     "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+   }
+ }
+
+ resource "aws_subnet" "demo" {
+   count = 2
+
+   availability_zone = data.aws_availability_zones.available.names[count.index]
+   cidr_block        = "10.0.${count.index}.0/24"
+   vpc_id            = aws_vpc.demo.id
+
+   tags = {
+     "Name"                                      = "terraform-eks-demo-node"
+     "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+   }
+ }
+
+ resource "aws_internet_gateway" "demo" {
+   vpc_id = aws_vpc.demo.id
+
+   tags = {
+     Name = "terraform-eks-demo"
+   }
+ }
+
+ resource "aws_route_table" "demo" {
+   vpc_id = aws_vpc.demo.id
+
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.demo.id
+   }
+ }
+
+ resource "aws_route_table_association" "demo" {
+   count = 2
+
+   subnet_id      = aws_subnet.demo[count.index].id
+   route_table_id = aws_route_table.demo.id
+ }
+
+# resource "aws_instance" "example" {
+#   ami           = "ami-b374d5a5"
+#   instance_type = "t2.micro"
+# }
+
+# resource "aws_eip" "ip" {
+#   vpc      = true
+#   instance = aws_instance.example.id
+# }
